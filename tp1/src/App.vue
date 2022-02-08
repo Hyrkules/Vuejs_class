992ae4e2f8a3e3d220a72c4dfff308b711ffbf13
hyrkules <helouribeucherie@gmail.com>
2022-02-08 16:06:00 +0100

    adding the first exercices of tp 1

---
+++ tp1/src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import { defineComponent } from 'vue'
+import Kilometrage from './components/Kilometrage.vue'
+export default defineComponent({
+  name: 'App',
+  components: {
+    Kilometrage
+  }
+});
+</script>
+
+<template>
+  <Kilometrage />
+</template>
---
+++ tp1/src/components/Kilometrage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="nom" /> Nom
+    <br>
+    <input type="text" v-model="prenom" /> Prenom
+    <br>
+    <input type="text" v-model="date" /> Date de naissance
+    <br>
+    <input type="text" v-model="place" /> Lieu
+    <button @click="showpage">Incrémenter</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { Value } from './Values.vue';
+
+export default defineComponent({
+  data: () => {
+    return {
+      Values
+    }
+  }
+});
+</script>
